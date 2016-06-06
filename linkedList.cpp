5aa3174adfd1080117840edc6280a3ff6232bf0a
Brian Campbell <briancmpbll@gmail.com>
2016-06-05 21:45:58 -0700

    Add push_front, pop_back, pop_front

--- linkedList.cpp
+++ linkedList.cpp
@@ -8,6 +8,8 @@ typedef shared_ptr<LinkedListNode> NodePtr;
 
 class LinkedListNode {
 public:
+  LinkedListNode(int _data) : data(_data) {};
+
   int data;
   shared_ptr<LinkedListNode> next;
 };
@@ -23,8 +25,7 @@ void printLinkedList(NodePtr head) {
 
 NodePtr push_back(NodePtr head, int data) {
   NodePtr current = head;
-  NodePtr newNode(new LinkedListNode);
-  newNode->data = data;
+  NodePtr newNode(new LinkedListNode(data));
 
   if (current == nullptr) {
     return newNode;
@@ -35,17 +36,72 @@ NodePtr push_back(NodePtr head, int data) {
   }
 
   current->next = newNode;
-
   return head;
+}
+
+NodePtr pop_back(NodePtr head) {
+  if (head == nullptr || head->next == nullptr) {
+    return nullptr;
+  }
+
+  NodePtr current = head;
+
+  while (current->next->next != nullptr) {
+    current = current->next;
+  }
+
+  current->next = nullptr;
+  return head;
+}
+
+NodePtr push_front(NodePtr head, int data) {
+  NodePtr newNode(new LinkedListNode(data));
+
+  newNode->next = head;
+
+  return newNode;
+}
+
+NodePtr pop_front(NodePtr head) {
+  if (head == nullptr) {
+    return head;
+  }
+
+  return head->next;
+}
+
+NodePtr delete_node(NodePtr head, int index) {
+  if (index < 0 || head == nullptr) {
+    return head;
+  }
+
+  // TODO
+  return nullptr;
 }
 
 int main(int argc, char **argv) {
   int numbers[] = {9, 5, 4, 10, 16, 8, 21, 46, 5};
-  NodePtr head;
+  NodePtr headBack;
+  NodePtr headFront;
 
   for (const int &x : numbers) {
-    head = push_back(head, x);
-    printLinkedList(head);
+    headBack = push_back(headBack, x);
+    printLinkedList(headBack);
+  }
+
+  while (headBack != nullptr) {
+    headBack = pop_back(headBack);
+    printLinkedList(headBack);
+  }
+
+  for (const int &x : numbers) {
+    headFront = push_front(headFront, x);
+    printLinkedList(headFront);
+  }
+
+  while (headFront != nullptr) {
+    headFront = pop_front(headFront);
+    printLinkedList(headFront);
   }
 
   return 0;
