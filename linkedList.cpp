3a358e2b8e0b6dd28e0359786e845a6aab23592e
Brian Campbell <briancmpbll@gmail.com>
2016-06-09 21:57:24 -0700

    Make LinkedList class

--- linkedList.cpp
+++ linkedList.cpp
@@ -3,105 +3,152 @@
 
 using namespace std;
 
-class LinkedListNode;
-typedef shared_ptr<LinkedListNode> NodePtr;
+template <class T>
+struct LinkedListNode;
 
-class LinkedListNode {
-public:
-  LinkedListNode(int _data) : data(_data) {};
+template <class T>
+using NodePtr = shared_ptr<LinkedListNode<T>>;
 
-  int data;
-  shared_ptr<LinkedListNode> next;
+template <class T>
+struct LinkedListNode {
+  LinkedListNode(T _data) : data(_data) {};
+
+  T data;
+  NodePtr<T> next;
+  NodePtr<T> prev;
 };
 
-void printLinkedList(NodePtr head) {
-  while (head != nullptr) {
-    cout << head->data << " ";
-    head = head->next;
+template <class T>
+class LinkedList {
+public:
+  LinkedList() : _size(0) {}
+
+  int size()  const {
+    return _size;
   }
 
-  cout << endl;
-}
+  void print() const {
+    NodePtr<T> current = _head;
+    while (current != nullptr) {
+      cout << current->data << " ";
+      current = current->next;
+    }
 
-NodePtr push_back(NodePtr head, int data) {
-  NodePtr current = head;
-  NodePtr newNode(new LinkedListNode(data));
-
-  if (current == nullptr) {
-    return newNode;
+    cout << endl;
   }
 
-  while (current->next != nullptr) {
-    current = current->next;
+  void print_reverse() const {
+    NodePtr<T> current = _tail;
+    while (current != nullptr) {
+      cout << current->data << " ";
+      current = current->prev;
+    }
+
+    cout << endl;
   }
 
-  current->next = newNode;
-  return head;
-}
+  void push_front(T data) {
+    NodePtr<T> newNode(new LinkedListNode<T>(data));
 
-NodePtr pop_back(NodePtr head) {
-  if (head == nullptr || head->next == nullptr) {
-    return nullptr;
+    if (_head != nullptr) {
+      _head->prev = newNode;
+    }
+
+    newNode->next = _head;
+    _head = newNode;
+    ++_size;
+
+    if (_size == 1) {
+      _tail = newNode;
+    }
   }
 
-  NodePtr current = head;
+  void pop_front() {
+    if (_head == nullptr) {
+      return;
+    }
 
-  while (current->next->next != nullptr) {
-    current = current->next;
+    _head = _head->next;
+    --_size;
+
+    if (_head != nullptr) {
+      _head->prev = nullptr;
+    }
+
+    if (_size <= 1) {
+      _tail = _head;
+    }
   }
 
-  current->next = nullptr;
-  return head;
-}
+  void push_back(T data) {
+    NodePtr<T> newNode(new LinkedListNode<T>(data));
 
-NodePtr push_front(NodePtr head, int data) {
-  NodePtr newNode(new LinkedListNode(data));
+    if (_tail != nullptr) {
+      _tail->next = newNode;
+    }
 
-  newNode->next = head;
+    newNode->prev = _tail;
+    _tail = newNode;
+    ++_size;
 
-  return newNode;
-}
-
-NodePtr pop_front(NodePtr head) {
-  if (head == nullptr) {
-    return head;
+    if (_size == 1) {
+      _head = newNode;
+    }
   }
 
-  return head->next;
-}
+  void pop_back() {
+    if (_tail == nullptr) {
+      return;
+    }
 
-NodePtr delete_node(NodePtr head, int index) {
-  if (index < 0 || head == nullptr) {
-    return head;
+    _tail = _tail->prev;
+    --_size;
+
+    if (_tail != nullptr) {
+      _tail->next = nullptr;
+    }
+
+    if (_size <= 1) {
+      _head = _tail;
+    }
   }
 
-  // TODO
-  return nullptr;
-}
+  void remove(int index) {
+    // TODO
+  }
+
+protected:
+  NodePtr<T> _head;
+  NodePtr<T> _tail;
+  int _size;
+};
 
 int main(int argc, char **argv) {
   int numbers[] = {9, 5, 4, 10, 16, 8, 21, 46, 5};
-  NodePtr headBack;
-  NodePtr headFront;
+  LinkedList<int> list;
 
   for (const int &x : numbers) {
-    headBack = push_back(headBack, x);
-    printLinkedList(headBack);
+    list.push_back(x);
+    list.print();
+    list.print_reverse();
   }
 
-  while (headBack != nullptr) {
-    headBack = pop_back(headBack);
-    printLinkedList(headBack);
+  while (list.size() > 0) {
+    list.pop_back();
+    list.print();
+    list.print_reverse();
   }
 
   for (const int &x : numbers) {
-    headFront = push_front(headFront, x);
-    printLinkedList(headFront);
+    list.push_front(x);
+    list.print();
+    list.print_reverse();
   }
 
-  while (headFront != nullptr) {
-    headFront = pop_front(headFront);
-    printLinkedList(headFront);
+  while (list.size() > 0) {
+    list.pop_front();
+    list.print();
+    list.print_reverse();
   }
 
   return 0;
