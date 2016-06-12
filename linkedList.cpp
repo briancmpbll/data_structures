00b46b70ddbd7394052fd86726168f2afd688cf1
Brian Campbell <briancmpbll@gmail.com>
2016-06-12 14:03:50 -0700

    Implement rest of linked list

--- linkedList.cpp
+++ linkedList.cpp
@@ -1,5 +1,6 @@
 #include <memory>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -47,6 +48,14 @@ public:
     cout << endl;
   }
 
+  int front() const {
+    if (_head == nullptr) {
+      throw string("No data");
+    }
+
+    return _head->data;
+  }
+
   void push_front(T data) {
     NodePtr<T> newNode(new LinkedListNode<T>(data));
 
@@ -78,6 +87,14 @@ public:
     if (_size <= 1) {
       _tail = _head;
     }
+  }
+
+  int back() {
+    if (_tail == nullptr) {
+      throw string("No data");
+    }
+
+    return _tail->data;
   }
 
   void push_back(T data) {
@@ -113,8 +130,60 @@ public:
     }
   }
 
+  void insert(T data, int index) {
+    if (index < 0 || index > _size) {
+      return;
+    }
+
+    if (index == 0) {
+      push_front(data);
+    }
+    else if (index == _size) {
+      push_back(data);
+    }
+    else {
+      NodePtr<T> newNode(new LinkedListNode<T>(data));
+
+      int count = 0;
+      NodePtr<T> current = _head;
+      while (count < index) {
+        current = current->next;
+        ++count;
+      }
+
+      newNode->prev = current->prev;
+      newNode->next = current;
+
+      current->prev->next = newNode;
+      current->prev = newNode;
+
+      ++_size;
+    }
+  }
+
   void remove(int index) {
-    // TODO
+    if (index < 0 || index >= _size) {
+      return;
+    }
+
+    if (index == 0) {
+      pop_front();
+    }
+    else if (index == _size - 1) {
+      pop_back();
+    }
+    else {
+      int count = 0;
+      NodePtr<T> current = _head;
+      while (count < index) {
+        current = current->next;
+        ++count;
+      }
+
+      current->prev->next = current->next;
+      current->next->prev = current->prev;
+      --_size;
+    }
   }
 
 protected:
@@ -124,27 +193,55 @@ protected:
 };
 
 int main(int argc, char **argv) {
-  int numbers[] = {9, 5, 4, 10, 16, 8, 21, 46, 5};
+  vector<int> numbers = {9, 5, 4, 10, 16, 8, 21, 46, 5};
   LinkedList<int> list;
 
+  try {
+    list.front();
+  }
+  catch (string e) {
+    cout << e << endl;
+  }
+
+  try {
+    list.back();
+  }
+  catch (string e) {
+    cout << e << endl;
+  }
+
+  cout << "///// push_back" << endl;
   for (const int &x : numbers) {
     list.push_back(x);
     list.print();
     list.print_reverse();
   }
 
+  cout << "///// insert" << endl;
+  list.insert(110000, 4);
+  list.print();
+  list.print_reverse();
+
+  cout << "///// pop_back" << endl;
   while (list.size() > 0) {
     list.pop_back();
     list.print();
     list.print_reverse();
   }
 
+  cout << "///// push_front" << endl;
   for (const int &x : numbers) {
     list.push_front(x);
     list.print();
     list.print_reverse();
   }
 
+  cout << "///// remove" << endl;
+  list.remove(5);
+  list.print();
+  list.print_reverse();
+
+  cout << "///// pop_front" << endl;
   while (list.size() > 0) {
     list.pop_front();
     list.print();
